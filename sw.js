c230628b25bacd5b5f5941fa740d477f6c8dde42
yaron amir <yaronamir86@gmail.com>
2026-03-04 12:07:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,20 @@
 
-/* Service Worker מינימלי (אופציונלי): Cache-first עבור קבצי האתר */
-const CACHE = "yaron-appraiser-site-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json"
-];
-
+const CACHE = "yaron-appraiser-pro-v1";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.json","./sw.js"];
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE ? null : caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k===CACHE ? null : caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  // רק GET
-  if(req.method !== "GET") return;
-
+  if(event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(req).then(hit => hit || fetch(req).then(res => {
-      // Try cache copy of same-origin resources
+    caches.match(event.request).then(hit => hit || fetch(event.request).then(res => {
       try{
-        const url = new URL(req.url);
+        const url = new URL(event.request.url);
         if(url.origin === location.origin){
-          const copy = res.clone();
-          caches.open(CACHE).then(cache => cache.put(req, copy));
+          caches.open(CACHE).then(c => c.put(event.request, res.clone()));
         }
       }catch(_){}
       return res;
